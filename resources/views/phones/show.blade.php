71a8dd25c09f901483b9655447f15cbcb481d6c9
anthonymerlier <webmaster@consor.fr>
2021-08-27 08:47:31 +0200

    add info phone show view

--- resources/views/phones/show.blade.php
+++ resources/views/phones/show.blade.php
@@ -3,4 +3,6 @@
 @section('content')
     <p>Modele : {{ $phone->name }}</p>
     <p>Marque : {{ $phone->brand->name }}</p>
+    <p>Année de sortie : {{ $phone->release_year }}</p>
+    <p>Pays de provenance : {{ $phone->country }}</p>
 @endsection
