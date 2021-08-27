49cbb4f0e13dd1ad56d4624ef18f1b7be75aa731
anthonymerlier <webmaster@consor.fr>
2021-08-27 08:48:43 +0200

    add brand country to phone show view

--- resources/views/phones/show.blade.php
+++ resources/views/phones/show.blade.php
@@ -4,5 +4,5 @@
     <p>Modele : {{ $phone->name }}</p>
     <p>Marque : {{ $phone->brand->name }}</p>
     <p>Année de sortie : {{ $phone->release_year }}</p>
-    <p>Pays de provenance : {{ $phone->country }}</p>
+    <p>Pays de provenance : {{ $phone->brand->country }}</p>
 @endsection
